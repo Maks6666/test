ecea6c58b1e866fc509b90b5e830862df9bd985c
Maks6666 <maxim.kucher2005@gmail.com>
2024-06-06 20:26:47 +0300

    1st part

--- main.py
+++ main.py
@@ -1,16 +1,93 @@
-# This is a sample Python script.
+# # 1
+# def summa(x, y):
+#     num = 0
+#     for i in range(x+1, y):
+#         num += i
+#     return num
+#
+# print(summa(2, 5))
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+# # 2
+# def summa():
+#     num = 0
+#     for i in range(1, 100):
+#         if i % 2 == 0:
+#             num += i
+#     return num
+#
+# print(summa())
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+# # 3
+# def string(string):
+#     res = ""
+#     for i in string:
+#         res += (f"{i} \n")
+#     return res
+# print(string("Hello"))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# # 4
+# def even_list(lst):
+#     new_lst = []
+#     for i in lst:
+#         if i % 2 == 0:
+#             new_lst.append(i)
+#     return new_lst
+#
+# print(even_list([2, 3, 4, 5, 6, 7]))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# # 5
+# def upper_strings(lst):
+#     new_lst = []
+#     for i in lst:
+#         if i[0] != i[0].lower():
+#             new_lst.append(i)
+#     return new_lst
+#
+# print(upper_strings(["Hello", "world", "Hi"]))
+
+
+# # 6
+# def python_strings(lst):
+#     new_lst = []
+#     for i in lst:
+#         if "Python" in i:
+#             new_lst.append(i)
+#     return new_lst
+#
+# print(python_strings(["Hello", "world", "Hi, Python", "python"]))
+
+# # 7
+# dictionary = {}
+#
+# def add_word(word, definition):
+#     dictionary[word] = definition
+#     print(f"{word}: {dictionary[word]} was added to the list")
+#
+# def del_word(word):
+#     if word in dictionary:
+#         del dictionary[word]
+#         print(f"{word} was deleted from the dictionary")
+#     else:
+#         print(f"{word} is not in the dictionary")
+#
+# def search(word):
+#     if word in dictionary:
+#         print(dictionary[word])
+#     else:
+#         print(f"{word} is not in the dictionary")
+#
+# add_word("Python", "Programming language")
+# del_word("Python-2")
+# search("Python")
+
+# # 8
+# lst = [(2, 1), (3, 8), (2, 2), (2, 4)]
+# sorted_lst = sorted(lst, key=lambda x: x[1])
+# print(sorted_lst)
+
+
+
+
